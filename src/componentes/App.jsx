4666016037304171d137143345298135d374dfa8
ReatiaRyMause <victormanuelarredondo054@gmail.com>
2025-05-22 19:19:35 -0600

    Cambios

---
+++ src/componentes/App.jsx
@@ -0,0 +1,20 @@
+import NavBar from '../componentes/NavBar';
+function App() {
+   return(
+       <>
+       <NavBar />
+        <div>
+           <h2>Bienvenido a mi página Web</h2>
+           <hr />
+           <p>
+               Este simple Aplicación es creado con React
+           </p>
+       </div>
+       </>
+   )
+
+
+}
+
+
+export default App;
